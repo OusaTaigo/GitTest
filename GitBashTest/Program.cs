76ab4e109e9d6f62289eb204da448b662fa997ab
Tiago Sousa <tiago.sousa85@hotmail.com>
2021-09-05 10:54:01 +0100

    Projecto Iniciado mostrando mensagem ola mundo

---
+++ GitBashTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBashTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string intro = "Hello World";
+            int id = 1;
+
+
+            Console.WriteLine(intro);
+            Console.WriteLine(id.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
