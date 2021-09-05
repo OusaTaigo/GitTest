6ed8180c2395524c1ccfb7567979950de3c64fef
Tiago Sousa <tiago.sousa85@hotmail.com>
2021-09-05 12:15:05 +0100

    Hella

--- GitBashTest/Program.cs
+++ GitBashTest/Program.cs
@@ -13,10 +13,11 @@ namespace GitBashTest
             string intro = "Hello World";
             int id = 1;
 
-
+            float novoCommitTest = 0.6f;
+            int hey = (int)Math.Floor(id - novoCommitTest);
             Console.WriteLine(intro);
             Console.WriteLine(id.ToString());
-
+            Console.WriteLine(hey.ToString());
             Console.ReadLine();
         }
     }
